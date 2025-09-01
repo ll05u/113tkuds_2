1f35c88dc3fef6d535e6e55b7efc4710bd360e02
ll05u <rita513513@gmail.com>
2025-09-01 10:42:59 +0800

    Create lt_08.java

---
+++ 0901/lt_08.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int myAtoi(String s) {
+        int i = 0, sign = 1, result = 0;
+        s = s.trim();
+        if (s.length() == 0) return 0;
+        if (s.charAt(i) == '+' || s.charAt(i) == '-') {
+            sign = s.charAt(i) == '-' ? -1 : 1;
+            i++;
+        }
+        while (i < s.length() && Character.isDigit(s.charAt(i))) {
+            int digit = s.charAt(i) - '0';
+            if (result > (Integer.MAX_VALUE - digit) / 10) {
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+            result = result * 10 + digit;
+            i++;
+        }
+        return result * sign;
+    }
+}
