4a12af4848bec89fd87e3e9e987030886204d412
ll05u <rita513513@gmail.com>
2025-09-01 10:40:29 +0800

    Create lt_04.java

---
+++ 0901/lt_04.java
@@ -0,0 +1,30 @@
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        if (nums1.length > nums2.length) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+        int m = nums1.length, n = nums2.length;
+        int imin = 0, imax = m, halfLen = (m + n + 1) / 2;
+        while (imin <= imax) {
+            int i = (imin + imax) / 2;
+            int j = halfLen - i;
+            if (i < m && nums2[j-1] > nums1[i]) {
+                imin = i + 1;
+            } else if (i > 0 && nums1[i-1] > nums2[j]) {
+                imax = i - 1;
+            } else {
+                int maxLeft = 0;
+                if (i == 0) maxLeft = nums2[j-1];
+                else if (j == 0) maxLeft = nums1[i-1];
+                else maxLeft = Math.max(nums1[i-1], nums2[j-1]);
+                if ((m + n) % 2 == 1) return maxLeft;
+                int minRight = 0;
+                if (i == m) minRight = nums2[j];
+                else if (j == n) minRight = nums1[i];
+                else minRight = Math.min(nums1[i], nums2[j]);
+                return (maxLeft + minRight) / 2.0;
+            }
+        }
+        return 0.0;
+    }
+}
