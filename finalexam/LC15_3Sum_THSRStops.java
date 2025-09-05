ec4b490f08e850968971b6cda9a7395b794c9834
ll05u <rita513513@gmail.com>
2025-09-05 12:55:31 +0800

    Update LC15_3Sum_THSRStops.java

---
+++ finalexam/LC15_3Sum_THSRStops.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+public class LC15_ThreeSum_THSRStations {
+// 讀入: n；下一行 n 整數；輸出: 所有和為 0 的不重複三元組，每行 a b c（遞增）
+public static void main(String[] args) throws Exception {
+BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+int n = Integer.parseInt(br.readLine().trim());
+int[] a = new int[n];
+StringTokenizer st = new StringTokenizer(br.readLine());
+for (int i = 0; i < n; i++) a[i] = Integer.parseInt(st.nextToken());
+Arrays.sort(a);
+StringBuilder out = new StringBuilder();
+for (int i = 0; i < n; i++) {
+if (i > 0 && a[i] == a[i-1]) continue;
+int L = i + 1, R = n - 1;
+while (L < R) {
+int sum = a[i] + a[L] + a[R];
+if (sum == 0) {
+out.append(a[i]).append(' ').append(a[L]).append(' ').append(a[R]).append('\n');
+int vL = a[L], vR = a[R];
+while (L < R && a[L] == vL) L++;
+while (L < R && a[R] == vR) R--;
+} else if (sum < 0) L++; else R--;
+}
+}
+System.out.print(out.toString());
+}
+}
