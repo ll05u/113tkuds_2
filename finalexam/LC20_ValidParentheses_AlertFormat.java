8771bb4e6afca6b4e3270c13ba046adac793b7ba
ll05u <rita513513@gmail.com>
2025-09-05 12:52:42 +0800

    Update LC20_ValidParentheses_AlertFormat.java

---
+++ finalexam/LC20_ValidParentheses_AlertFormat.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+public class LC04_ValidBrackets_Emergency {
+
+public static void main(String[] args) throws Exception {
+BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+String s = br.readLine();
+Deque<Character> st = new ArrayDeque<>();
+for (char c : s.toCharArray()) {
+if (c == '(' || c == '[' || c == '{') st.push(c);
+else {
+if (st.isEmpty()) { System.out.println(false); return; }
+char p = st.pop();
+if (!match(p, c)) { System.out.println(false); return; }
+}
+}
+System.out.println(st.isEmpty());
+}
+static boolean match(char a, char b) {
+return (a=='('&&b==')')||(a=='['&&b==']')||(a=='{'&&b=='}');
+}
+}
